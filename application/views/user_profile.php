2b3e04dc1b96875f3180f48b52efdd3443c2d876
Shujaat Ali <m.ali.hussan.hussain@gmail.com>
2015-06-04 23:46:33 +0500

    added profile page

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -1,1 +1,8 @@
-<?=$username?>
+<div class="container wrapper">
+	<div class="user_pic_div">
+			<?php if(isset($pic)) {echo "will add soon";}
+					else{?><img src="<?=base_url();?>assets/imgs/no-img.jpg" class="user_pic"><?php } ?>
+		</div> <!-- user_pic_div -->
+	<div class="user_info">
+	</div> <!-- user_info -->
+</div> <!-- container -->
