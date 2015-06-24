6cf1f58310f1ac88d817aba6ca34dd1cb7bedb16
Shujaat Ali <m.ali.hussan.hussain@gmail.com>
2015-06-25 04:02:27 +0500

    save and edit buttons added plus disabled all texboxes so no one can edit them

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -12,30 +12,32 @@
 		</ul> <!-- unav nav-tabs -->
 
 		<div class="user_info_load" id="personal_info" onclick="changeText(this)">
-			<table><tr><td><span class="user_info_label">Name</span></td><td><input type="text" class="form-control user_info_text" placeholder="<?php if(isset($name)){echo $name;} else {echo "No Name";} ?>" aria-describedby="basic-addon2"></td></tr>
-				<tr><td><span class="user_info_label">Username</span></td><td><input type="text" class="form-control user_info_text" placeholder="<?php if(isset($username)){echo $username;} else{echo "No Username";} ?>" aria-describedby="basic-addon2"></td></tr>
-				<tr><td><span class="user_info_label">Sex</span></td><td><input type="text" class="form-control user_info_text" placeholder="<?php if($sex == "1"){echo "Male";}  if($sex == "2"){echo "Female";}  if($sex == "0"){ echo "No Sex Provided";}?>" aria-describedby="basic-addon2"></td></tr>
-				<tr><td><span class="user_info_label">About</span></td><td><textarea id="txtArea" rows="10" cols="70" class="form-control user_info_text"><?php if(isset($about)){echo $about;} else{echo "No About Myself";} ?></textarea></td></tr>
+			<table><tr><td><span class="user_info_label">Name</span></td><td><input type="text" class="form-control user_info_text" placeholder="<?php if(isset($name)){echo $name;} else {echo "No Name";} ?>" aria-describedby="basic-addon2" disabled></td></tr>
+				<tr><td><span class="user_info_label">Username</span></td><td><input type="text" class="form-control user_info_text" placeholder="<?php if(isset($username)){echo $username;} else{echo "No Username";} ?>" aria-describedby="basic-addon2" disabled></td></tr>
+				<tr><td><span class="user_info_label">Sex</span></td><td><input type="text" class="form-control user_info_text" placeholder="<?php if($sex == "1"){echo "Male";}  if($sex == "2"){echo "Female";}  if($sex == "0"){ echo "No Sex Provided";}?>" aria-describedby="basic-addon2" disabled></td></tr>
+				<tr><td><span class="user_info_label">About</span></td><td><textarea id="txtArea" disabled rows="10" cols="70" class="form-control user_info_text"><?php if(isset($about)){echo $about;} else{echo "No About Myself";} ?></textarea></td></tr>
 			</table>
 			 
 		</div> <!-- user_info_load -->
 
 		<div class="user_info_load " id="user_edu_work" >
-			<table><tr><td><span class="user_info_label">Work</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($work)){echo $work;} else {echo "No Occupation Given";} ?>" aria-describedby="basic-addon2" id="restoff_user_info_labels"></td></tr>
-				<tr><td><span class="user_info_label">University</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($comapny)){echo $comapny;} else {echo "No Company Name Provided";} ?>" aria-describedby="basic-addon2 " id="restoff_user_info_labels"></td></tr>
-				<tr><td><span class="user_info_label">University</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($uni)){ echo $uni; } else{ echo "No University Provided";}?>" aria-describedby="basic-addon2 " id="restoff_user_info_labels"></td></tr>
-				<tr><td><span class="user_info_label">High School</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($collage)){ echo $collage; } else{ echo "No Colalge Provided";}?>" aria-describedby="basic-addon2" id="restoff_user_info_labels"></td></tr>
+			<table><tr><td><span class="user_info_label">Work</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($work)){echo $work;} else {echo "No Occupation Given";} ?>" aria-describedby="basic-addon2" id="restoff_user_info_labels" disabled></td></tr>
+				<tr><td><span class="user_info_label">University</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($comapny)){echo $comapny;} else {echo "No Company Name Provided";} ?>" aria-describedby="basic-addon2 " id="restoff_user_info_labels" disabled></td></tr>
+				<tr><td><span class="user_info_label">University</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($uni)){ echo $uni; } else{ echo "No University Provided";}?>" aria-describedby="basic-addon2 " id="restoff_user_info_labels" disabled></td></tr>
+				<tr><td><span class="user_info_label">High School</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($collage)){ echo $collage; } else{ echo "No Colalge Provided";}?>" aria-describedby="basic-addon2" id="restoff_user_info_labels" disabled></td></tr>
 			</table>
 			 
 		</div> <!-- user_info_load -->
 
 		<div class="user_info_load " id="user_contact">
-			<table><tr><td><span class="user_info_label">Address</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($address)){ echo $address; } else{ echo "No Address Provided"; }?>" aria-describedby="basic-addon2" id="restoff_user_info_labels"></td></tr>
-				<tr><td><span class="user_info_label">City</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($city)){ echo $city;} else{ echo "No City Provided";}?>" aria-describedby="basic-addon2" id="restoff_user_info_labels"></td></tr>
-				<tr><td><span class="user_info_label">Country</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($country)){ echo $country;} else{ echo " No Country Provided";}?>" aria-describedby="basic-addon2" id="restoff_user_info_labels"></td></tr>
+			<table><tr><td><span class="user_info_label">Address</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($address)){ echo $address; } else{ echo "No Address Provided"; }?>" aria-describedby="basic-addon2" id="restoff_user_info_labels" disabled></td></tr>
+				<tr><td><span class="user_info_label">City</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($city)){ echo $city;} else{ echo "No City Provided";}?>" aria-describedby="basic-addon2" id="restoff_user_info_labels" disabled></td></tr>
+				<tr><td><span class="user_info_label">Country</span></td><td><input type="text" class="form-control " placeholder="<?php if(isset($country)){ echo $country;} else{ echo " No Country Provided";}?>" aria-describedby="basic-addon2" id="restoff_user_info_labels" disabled></td></tr>
 			</table>
 			 
 		</div> <!-- user_info_load -->
-
+		<div class="profil_scrn_btns_div">
+			<button class="btn btn-default profile_scrn_btns">Edit</button><button class="btn btn-default profile_scrn_btns">Save</button>
+			</div>
 	</div> <!-- user_info -->
 </div> <!-- container -->
