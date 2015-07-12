ea8ad0b084fc5aaa5d258146a0cdecf058c2ddaa
Shujaat Ali <m.ali.hussan.hussain@gmail.com>
2015-07-12 18:53:33 -0500

    added function in the model to get suggestions from the db

--- application/models/get_suggestions.php
+++ application/models/get_suggestions.php
@@ -8,10 +8,16 @@ class get_suggestions extends CI_Model{
 	 	if(isset($usersrqst)){
 	 		$result = $this->db->query("SELECT * from users WHERE name LIKE '".$query."%'");
 			if($result->num_rows()>0){
-
 				$row = $result->row();
-				$response['name'] = $row->name;
-				$response['id'] = $row->id; 
+				for($i=0; $i<$result->num_rows(); $i++){
+
+					$response[$i]["name"]=$row->name;
+					$response[$i]["username"]= $row->username;
+					$row = $result->next_row();
+				}
+
+				
+				
 
 				return $response;
 			}
